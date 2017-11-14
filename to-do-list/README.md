78a9a3b8f6b266423414be679f86fc892de67f10
Krystian Jabłoński <Krystian Jaboski>
2017-11-14 01:18:14 +0100

    initial

---
+++ to-do-list/README.md
@@ -0,0 +1,9 @@
+#React To Do List
+
+App made with use of `React Router`.
+
+To use, you need a `Webpack`.
+
+Run by `npm start`.
+
+Made with help of Shaun, who is a coolest front-end developer from the UK.
---
+++ to-do-list/src/app/index.js
@@ -0,0 +1,70 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+import { Router, Route, browserHistory, Link } from 'react-router';
+
+//Module requires
+var TodoItem = require('./todoItem');
+var AddItem = require('./addItem');
+var About = require('./about');
+
+//CSS requires.
+require('./css/index.css');
+
+
+//Routing setup.
+//borwserHistory enables to move through '/' with the use of browser arrows.
+var App = React.createClass({
+    render: function(){
+        return(
+            <Router history={browserHistory}>
+                <Route path={"/"} component={TodoComponent}></Route>
+                <Route path={"/about"} component={About}></Route>
+            </Router>
+        );
+    }
+});
+
+//Create a component.
+var TodoComponent = React.createClass({
+    getInitialState: function(){
+        return {
+            todos: ['wash up', 'eat some cheese', 'take a nap']
+        }
+    }, //Initial state of the array.
+    render: function(){
+        var todos = this.state.todos;
+        todos = todos.map(function(item, index){
+            return(<TodoItem key={index} item={item} onDelete={this.onDelete} />);
+        }.bind(this));
+        return(
+            <div id="todo-list">
+                <Link to={"/about"}>About</Link>
+                <p>Are you busy?</p>
+                <ul>{todos}</ul>
+                <AddItem onAdd={this.onAdd} />
+            </div>
+        );
+    },
+
+    //Custom functions.
+    //Responsible for taking handled values and putting them into array.
+    onDelete: function(item){
+        var updatedTodos = this.state.todos.filter(function(val, index){
+            return item !== val;
+        });
+        this.setState({
+          todos: updatedTodos
+        });
+    },
+
+    onAdd: function(item){
+        var updatedTodos = this.state.todos;
+        updatedTodos.push(item);
+        this.setState({
+            todos: updatedTodos
+        })
+    }
+
+});
+
+ReactDOM.render(<App />, document.getElementById('todo-wrapper'));
