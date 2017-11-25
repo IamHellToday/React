46cecb42c9b3f834cc16d2dba9cbbd6928ede368
Krystian Jabłoński <Krystian Jaboski>
2017-11-25 15:57:36 +0100

    fixes

--- components/Contacts.js
+++ components/Contacts.js
@@ -3,7 +3,7 @@ const Contacts = React.createClass({
         items: React.PropTypes.array.isRequired,
     },
     render(){
-        let contacts = this.props.items.map(contact => React.createElement(Contact, {item: contact, key: contact.id}));
+        let contacts = this.props.items.map(contact => <Contact item={contact} key={contact.id} />);
 
         return <ul className='contactList'>{contacts}</ul>
     }
